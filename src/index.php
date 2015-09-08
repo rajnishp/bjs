59fc7d49a9ac8071aa1ec3bed95d15962dedefba
collap <rajnish_pawar90@yahoo.com>
2015-09-08 12:48:44 +0530

    router and home controller

--- src/index.php
+++ src/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 // server should keep session data for AT LEAST 1 hour
 ini_set('session.gc_maxlifetime', 728000);
@@ -6,20 +7,9 @@ ini_set('session.gc_maxlifetime', 728000);
 // each client should remember their session id for EXACTLY 1 hour
 session_set_cookie_params(728000);
 
-session_start();
+
 
 include_once "controllers/HomeController.class.php";
-include_once "controllers/ProjectController.class.php";
-include_once "controllers/ActivityController.class.php";
-include_once "controllers/ProfileController.class.php";
-include_once "controllers/DashboardController.class.php";
-include_once "controllers/SettingController.class.php";
-include_once "controllers/CompleteProfileController.class.php";
-include_once "controllers/PushFormsController.class.php";
-include_once "controllers/RecoverPasswordController.class.php";
-
-
-//include_once "components/base.php";
 
 
 require_once 'utils/Util.php';
@@ -30,28 +20,19 @@ require_once 'cache/AppCacheRegistry.class.php';
 /*
 
 type
-1.		Login/registor (Landing Page)
-2.		afterlogin (Single page app)
-		2.a 	Dashbord
-		2.b 	Project Page
-		2.c 	Profile Page
-		2.d 	openChall Page
+1.		Landing Page
 
-3.		profile page
-4.		Project page
-5.		OpenChange page
-
-6. 		Search Page *****
+2. 		Search Page *****
 ..................................
-job.collap.com
+admin.blueteam.com
 
-org.collap.com
+operate.blueteam.com
 
 */
 
 /* Setting up the app-configurations globally for use across classes */
 global $configs;
-$configs = json_decode (file_get_contents('collap-configs.json'), true);
+$configs = json_decode (file_get_contents('blueteam-configs.json'), true);
 
 /* Setting up the logger globally for use across classes */
 global $logger;
@@ -66,7 +47,7 @@ $route = explode("/",$_SERVER[REQUEST_URI]);
 
 //router hack for uploads
 if(in_array('uploads', $route)){
-	$redir = $configs['COLLAP_BASE_URL'];
+	$redir = $configs['BLUETEAM_BASE_URL'];
 	$flag = false;
 
 	foreach ($route as $key => $value) {
@@ -86,416 +67,45 @@ $logger -> debug ("post :: " .json_encode($_POST));
 
 $logger -> debug ("get :: " .json_encode($_GET));
 
-//for url translantion
-if(substr($route[1], 0, 11) == "profile.php"){
-	$temp = explode("=", $route[1]);
-	$route[2] = $temp[1];
-	$route[1] = "profile";
-
-}
-if(substr($route[1], 0, 18) == "challengesOpen.php"){
-	$temp = explode("=", $route[1]);
-	$route[2] = $temp[1];
-	$route[1] = "activity";
-	
-}
-if(substr($route[1], 0, 11) == "project.php"){
-	$temp = explode("=", $route[1]);
-	$route[2] = $temp[1];
-	$route[1] = "project";
-	
-}
-//end of url translantion
-
-if ( ! isset($_SESSION['user_id']) && count($route) <= 1  ){
-	//langing page of collap
-	$homeController = new HomeController();
-	$homeController -> render ();
-}else {
 
 		$page = $route[1];
 		//single page app
 		switch ($page) {
 
-			case "project":
-										
-					$projectController = new ProjectController($route[2]);
-					$where = $route[3];
-					
-					switch ($where) {
-							case 'activities':
-								
-								$projectController -> getNextActivities();
-
-								break;
-
-							case 'activity':
-								$dashboardController -> postActivity ();
-								break;
-
-							case 'logout':
-								$homeController -> logout ();
-								break;
-
-							case 'forgetPassword':
-								$homeController -> forgetPassword ();
-								break;
-							
-							default:
-
-								if ($route['2'] == 'createNew'){
-
-									$projectController -> createNewProject();
-								}
-
-								elseif($route['2'] == 'createProject') {
-
-									$projectController -> createProject();
-								}
-
-								elseif ($route['2'] == 'postProjectComment') {
-									$projectController -> postProjectComment();
-								}
-
-								elseif ($route['3'] == 'joinProject') {
-									$projectController -> joinProject();
-								}
-
-								else 
-									$projectController -> render ();
-	
-								break;
-					}
-				break;
-
-			case "activity":
-
-					$activityController = new ActivityController($route[2]);
-					
-					$where = $route[2];
-								
-					switch ($where) {
-						case 'postComment':
-							
-							$activityController -> postComment();
-
-							break;
-						default:
-							$activityController -> render();
-							break;
-					}
-				break;
 
 			case "fileUpload":
 				require_once "controllers/FilesController.class.php";
 					
 				break;
 
-			case "profile":
-
-					$profileController = new ProfileController($route[2]);
-
-					$where = $route[3];
-
-					switch ($where) {
-							case 'activities':
-								
-								$profileController -> getNextActivities();
-
-								break;
-
-							case 'ideas':
-								$profileController -> getNextIdeas();
-								break;
-
-							case 'projects':
-								$profileController -> getNextProjects();
-								break;
-
-							case 'forgetPassword':
-								$homeController -> forgetPassword ();
-								break;
-
-
-							case 'sendLinkRequest':
-								$profileController -> sendLinkRequest();
-								break;					
-
-							case 'confirmLinkRequest':
-								$profileController -> confirmLinkRequest();
-								break;	
-
-							case 'deleteLinkRequest':
-								$profileController -> deleteLinkRequest();
-								break;	
-							
-							default:
-								if($route[2] == 'activities')
-									$profileController -> getNextActivities();
-								else if($route[2] == 'ideas')
-									$profileController -> getNextIdeas();
-								else if($route[2] == 'projects')
-									$profileController -> getNextProjects();
-								else
-									$profileController -> render ();
-								break;
-					}
-				break;
-
-			case "dashboard":
-
-					$dashboardController = new DashboardController($route[2]);
-					$where = $route[2];
-					
-					switch ($where) {
-							case 'activities':
-								
-								$dashboardController -> getNextActivities();
-
-								break;
-
-							case 'postActivity':
-								$dashboardController -> postActivity ();
-								break;
-
-							case 'forgetPassword':
-								$homeController -> forgetPassword ();
-								break;
-
-							case 'pushForm':
-								$pushFormController = new PushFormsController($route[2]);
-								$pushFormController ->  pushForm();
-								break;
-
-							default:
-								$dashboardController -> render ();
-								break;
-					}
-				
-				break;
-
-			case "setting":
-
-					$settingController = new SettingController();
-					$where = $route[2];
-					switch ($where) {
-							case 'updateTechStrength':
-								
-								$settingController -> updateTechStrength();
-
-								break;
-
-							case 'updateUserInfo':
-								
-								$settingController -> updateUserInfo();
-
-								break;
-
-							case 'updateWorkExp':
-								
-								$settingController -> updateWorkExp();
-
-								break;
-							case 'updateEducation':
-								
-								$settingController -> updateEducation();
-
-								break;
-								
-							case 'updateSkills':
-								
-								$settingController -> updateSkills();
-
-								break;
-								
-							case 'removeSkill':
-								
-								$settingController -> removeSkill();
-
-								break;
-
-							case 'removeLocation':
-								
-								$settingController -> removeLocation();
-
-								break;
-							
-							case 'updateLocations':
-								
-								$settingController -> updateLocations();
-
-								break;
-
-							case 'updateJobPreference':
-								
-								$settingController -> updateJobPreference();
-
-								break;
-								
-							case 'updateSocialLinks':
-								
-								$settingController -> updateSocialLinks();
-
-								break;
-
-							case 'updatePassword':
-								
-								$settingController -> updatePassword();
-
-								break;								
-
-							case 'signup':
-								$homeController -> signup ();
-								break;
-
-							case 'logout':
-								$homeController -> logout ();
-								break;
-
-							case 'forgetPassword':
-								$homeController -> forgetPassword ();
-								break;
-							
-							default:
-								$settingController -> render();
-								break;
-						}
-
-				break;
 
 			case "home":
-					$homeController = new HomeController();
-					if($route[2] == "logout"){
-						$homeController -> logout ();
-					}
-					elseif($route[2] == 'signup') {
-						$homeController -> signup ();
-					}
-					elseif($route[2] == 'login') {
-						$homeController -> login ();
-					}
-					elseif($route[2] == 'forgetPassword') {
-						$homeController -> forgetPassword ();
-					}
+				$homeController = new HomeController();
+				$where = $route[2];
+
+				switch ($page) {
+
+					case '':
+						$homeController ->  home();
+						break;
 					
-					if (!empty($_POST)){
-						$form = $_POST['submit'];
-
-						switch ($form) {
-							case 'login':
-								
-								$homeController -> login ();
-								break;
-
-							case 'signup':
-								$homeController -> signup ();
-								break;
-
-							case 'logout':
-								$homeController -> logout ();
-								break;
-
-							case 'forgetPassword':
-								$homeController -> forgetPassword ();
-								break;
-							
-							default:
-								$homeController -> render ();
-								break;
-						}
-
-					}
-					else{
-						$checkNewRoute = explode("=",$route[2]);
-						
-						if ($checkNewRoute[0] == 'usernameCheck') {
-							$homeController -> usernameCheck (urldecode($checkNewRoute[1]));
-						}
-						elseif($checkNewRoute[0] == 'emailCheck') {
-							$homeController -> emailCheck (urldecode($checkNewRoute[1]));
-						}
-						else {
-							$homeController -> render ();
-						}
-					}
+					default:
+						$homeController -> render ();
+						break;
+				}
 
 				break;
-			
-			case "completeProfile":
-					$completeProfileController = new CompleteProfileController();
-
-					if($route[2] == "finish"){
-						$completeProfileController -> finishCompleteProfile();
-						break;
-					}
-
-					if( isset($_SESSION["user_id"] )){
-						
-						$completeProfileController -> render();
-						break;
-					}
 				
 
 			default:
 
-					$recoverPasswordReq = explode('?', $page);
-					
-					if ($recoverPasswordReq[0] == "forgetPassword") {
-						//var_dump($recoverPasswordReq); die();
-						$recoverPasswordController = new RecoverPasswordController();
-						
-						//$routeHashKeyCheck = explode('?', $recoverPasswordReq[1]);
-						
-						//var_dump($routeHashKeyCheck); die();
+				//langing page of blueteam
+				// Can also be routed to 404 page
+				$homeController = new HomeController();
+				$homeController -> render ();
 
-						if($route[2] == "newPassword"){
-							
-							$previousRoute = explode("/",$_SERVER['HTTP_REFERER']);
-							$previousPage = explode('?', $previousRoute[3]);
-							$hashKey = explode('=', $previousPage[1]);
-							$haskKeyAidId = explode('.', $hashKey[1]);
-							
-							$recoverPasswordController -> updatePassword($haskKeyAidId[0], $haskKeyAidId[1]);
-							break;
-						}
-						else {
-							$hashKey = explode('=', $recoverPasswordReq[1]);
-							$haskKeyAidId = explode('.', $hashKey[1]);
-							$recoverPasswordController->render($haskKeyAidId[0], $haskKeyAidId[1]);
-						}
-						break;
-					}
-
-					if( isset($_SESSION["user_id"] )){
-						$dashboardController = new DashboardController();
-						
-						if($route[2] == 'activities')
-							$dashboardController -> getNextActivities();
-
-						elseif ($route[2] == 'postActivity')
-								$dashboardController -> postActivity ();
-
-						else
-							$dashboardController -> render ();
-						break;
-					} 
-
-					else {
-						//langing page of collap 
-						// Can also be routed to 404 page
-						$homeController = new HomeController();
-						$homeController -> render ();
-
-					}
-				
 				break;
+
 		}
-
-	
-}
-
-
-
-
 
 ?>
--- src/models/Project.class.php
+++
@@ -1,332 +0,0 @@
-<?php
-	/**
-	 * Object represents table 'projects'
-	 *
-     	 * @author: Rahu Lhaoria
-     	 * @date: 2015-03-03 14:48	 
-	 */
-
-	require_once 'models/BaseModel.class.php';
-	class Project extends BaseModel {
-		
-		private $id;
-		private $userId;
-		private $blobId;
-		private $projectTitle;
-		private $stmt;
-		private $type;
-		private $orgId;
-		private $order;
-		private $creationTime;
-		private $projectValue;
-		private $fundNeeded;
-		private $lastUpdateTime;
-
-		private $technicalSkills;
-		private $myRole;
-		private $teamSize;
-		private $durationFrom;
-		private $durationTo;
-
-		private $status;
-
-		private $firstName;
-		private $lastName;
-		private $username;
-		private $responses;
-
-	
-		function __construct ($userId, $blobId, $projectTitle, $stmt, $type, $orgId, $order, 
-								$creationTime, $projectValue, $fundNeeded, $lastUpdateTime,
-								$technicalSkills, $myRole, $teamSize, $durationFrom, $durationTo, $status,
-								$firstName, $lastName, $username, $id = null) {
-			$this -> id = $id;
-			$this -> userId = $userId;
-			$this -> blobId = $blobId;
-			$this -> projectTitle = $projectTitle;
-			$this -> stmt = $stmt;
-			$this -> type = $type;
-			$this -> orgId = $orgId;
-			$this -> order = $order;
-			$this -> creationTime = $creationTime;
-			$this -> projectValue = $projectValue;
-			$this -> fundNeeded = $fundNeeded;
-			$this -> lastUpdateTime = $lastUpdateTime;
-			
-			$this -> technicalSkills = $technicalSkills;
-			$this -> myRole = $myRole;
-			$this -> teamSize = $teamSize;
-			$this -> durationFrom = $durationFrom;
-			$this -> durationTo = $durationTo;
-
-			$this -> status = $status;
-
-			$this -> firstName = $firstName;
-			$this -> lastName = $lastName;
-			$this -> username = $username;
-
-		}
-		
-
-		function setId ($id) {
-			$this -> id = $id;
-		}
-		function getId () {
-			return $this -> id;
-		}
-
-		function getRefinedStmt(){
-			//repalace space tages
-			$stmt = $this->replaceTags($this->stmt);
-			
-			if ($stmt[0] == "<"){
-
-				$first=explode(' ', $stmt);
-				$rest=ltrim($stmt, $first[0]);
-				$stmt = $first[0] . " class=\"post-img img-responsive\" " . $rest; 
-			}
-
-			return $stmt;
-
-
-		}
-
-		private function replaceTags($req){
-
-			return str_replace(
-							"<s>", "&nbsp;", 
-								str_replace("<r>", "'", 
-										str_replace("<a>", "&",
-											str_replace("<an>", "+", $req))));
-
-		}
-
-		function getKeywords(){
-			return $this->extract_keywords($this->getRefinedTitle() ." ". $this->getRefinedStmt());
-		}
-
-		function getImage(){
-		if ($this->stmt[0] == "<")
-			$temp = explode("\"", $this->stmt);
-		return $temp[1];
-		}
-
-		function getRefinedTitle(){
-			//repalace space tages
-			$stmt = $this->replaceTags($this->projectTitle);
-			
-			if ($stmt[0] == "<"){
-
-				$first=explode(' ', $stmt);
-				$rest=ltrim($stmt, $first[0]);
-				$stmt = $first[0] . " class=\"post-img img-responsive\" " . $rest; 
-			}
-
-			return $stmt;
-
-
-		}
-
-
-		function setBlobId ($blobId) {
-			$this -> blobId = $blobId;
-		}
-		function getBlobId () {
-			return $this -> blobId;
-		}
-
-		function setUserId ($userId) {
-			$this -> userId = $userId;
-		}
-		function getUserId () {
-			return $this -> userId;
-		}
-
-		function setProjectTitle ($projectTitle) {
-			$this -> projectTitle = $projectTitle;
-		}
-		function getProjectTitle () {
-			return $this -> projectTitle;
-		}
-
-		function setStmt ($stmt) {
-			$this -> stmt = $stmt;
-		}
-		function getStmt () {
-			return $this -> stmt;
-		}
-
-		function setType ($type) {
-			$this -> type = $type;
-		}
-		function getType () {
-			return $this -> type;
-		}
-
-		function setOrgId ($orgId) {
-			$this -> orgId = $orgId;
-		}
-		function getOrgId () {
-			return $this -> orgId;
-		}
-
-		function setOrder ($order) {
-			$this -> order = $order;
-		}
-		function getOrder () {
-			return $this -> order;
-		}
-
-		function setCreationTime ($creationTime) {
-			$this -> creationTime = $creationTime;
-		}
-		function getCreationTime () {
-			return $this -> creationTime;
-		}
-
-		function setProjectValue ($projectValue) {
-			$this -> projectValue = $projectValue;
-		}
-		function getProjectValue () {
-			return $this -> projectValue;
-		}
-
-		function setLastUpdateTime ($lastUpdateTime) {
-			$this -> lastUpdateTime = $lastUpdateTime;
-		}
-		function getLastUpdateTime () {
-			return $this -> lastUpdateTime;
-		}
-
-		function setFundNeeded ($fundNeeded) {
-			$this -> fundNeeded = $fundNeeded;
-		}
-		function getFundNeeded () {
-			return $this -> fundNeeded;
-		}
-
-
-		function setTechnicalSkills ($technicalSkills) {
-			$this -> technicalSkills = $technicalSkills;
-		}
-		function getTechnicalSkills () {
-			return $this -> technicalSkills;
-		}
-
-		function setMyRole ($myRole) {
-			$this -> myRole = $myRole;
-		}
-		function getMyRole () {
-			return $this -> myRole;
-		}
-
-		function setTeamSize ($teamSize) {
-			$this -> teamSize = $teamSize;
-		}
-		function getTeamSize () {
-			return $this -> teamSize;
-		}
-
-		function setDurationFrom ($durationFrom) {
-			$this -> durationFrom = $durationFrom;
-		}
-		function getDurationFrom () {
-			return $this -> durationFrom;
-		}
-
-		function setDurationTo ($durationTo) {
-			$this -> durationTo = $durationTo;
-		}
-		function getDurationTo () {
-			return $this -> durationTo;
-		}
-
-
-		function setStatus($status){
-			$this -> status = $status;
-		}
-		function getStatus(){
-			return $this-> status;
-		}
-
-
-		function setFirstName($firstName){
-			$this -> firstName = $firstName;
-		}
-		function getFirstName(){
-			return $this-> firstName;
-		}
-		
-		function setLastName($lastName){
-			$this -> lastName = $lastName;
-		}
-		function getLastName(){
-				return $this->lastName;
-		}
-
-		function setUsername($username){
-			$this -> username = $username;
-		}
-		function getUsername(){
-				return $this-> username;
-		}
-
-		function setResponses($responses){
-			$this -> responses= $responses;
-		}
-		function getResponses(){
-			return $this-> responses;		
-		}
-
-		function toString() {
-			return $this -> id . ", " . 
-					$this -> userId.",".
-					$this -> blobId.",".
-					$this -> projectTitle.",".
-					$this -> stmt.",".
-					$this -> type . ", " . 
-					$this -> orgId.",".
-					$this -> order.",".
-					$this -> creationTime.".".
-					$this -> projectValue. ", " . 
-					$this -> fundNeeded.",".
-					$this -> lastUpdateTime.",".
-					$this -> firstName.",".
-					$this -> lastName.",".
-					$this -> userName;
-		}
-
-		function toArray() {
-			return array (			
-							id => $this-> id,
-							userId => $this-> userId,
-							blobId => $this-> blobId,
-							projectTitle => $this-> projectTitle,
-							stmt => $this-> stmt,
-							type => $this-> type,
-							orgId => $this-> orgId,
-							order => $this-> order,
-							creationTime => $this-> creationTime,
-							projectValue => $this-> projectValue,
-							fundNeeded => $this-> fundNeeded,
-							lastUpdateTime => $this-> lastUpdateTime,
-							firstName => $this-> firstName,
-							lastName => $this-> lastName,
-							userName => $this-> userName
-						);
-		}
-
-		function toArrayUserProjects() {
-			return array (			
-							id => $this-> id,
-							projectTitle => $this-> projectTitle,
-							stmt => $this-> stmt,
-							type => $this-> type,
-							creationTime => $this-> creationTime,
-							firstName => $this-> firstName,
-							lastName => $this-> lastName,
-							userName => $this-> userName
-						);
-		}
-	}
-?>
