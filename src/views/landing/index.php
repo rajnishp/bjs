a5c12455629cda084658b6b856355ff6af140a87
collap <rajnish_pawar90@yahoo.com>
2015-09-10 19:12:16 +0530

    fevocon added

--- src/views/landing/index.php
+++ src/views/landing/index.php
@@ -16,7 +16,24 @@
 
 
         <!-- Favicon -->
-        <link href="#" rel="icon" type="image/png">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="<?= $this-> baseUrl ?>static/images/fevicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?= $this-> baseUrl ?>static/images/fevicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="<?= $this-> baseUrl ?>static/images/fevicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?= $this-> baseUrl ?>static/images/fevicon/favicon-16x16.png">
+        <link rel="manifest" href="<?= $this-> baseUrl ?>static/images/fevicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="<?= $this-> baseUrl ?>static/images/fevicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
 
         <!-- Assets -->
         <link rel="stylesheet" href="<?= $this-> baseUrl ?>static/css/owl.css">
