ab444f5b658eb8b93ebd7b75911fe69281d0ea40
Rajnish Panwar <rajnish_pawar90@yahoo.com>
2016-01-04 15:47:53 +0530

    testimonials

--- src/views/testimonials/addtestimonial.php
+++ src/views/testimonials/addtestimonial.php
@@ -1,5 +1,5 @@
 <?php 
-  $db_handle = mysqli_connect("localhost","root","redhat111111","bluenethack");
+  $db_handle = mysqli_connect("localhost","root","redhat@11111p","bluenethack");
 
 //Check connection
   if (mysqli_connect_errno()) {
