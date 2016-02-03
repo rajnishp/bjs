85dc8ee9e5b60e7edce9b86d41088f7c5fe99087
anil-ninja <kumar.anil8892@gmail.com>
2016-02-03 15:44:29 +0530

    varification

---
+++ src/controllers/VarifiedController.class.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'controllers/BaseController.class.php';
+require_once 'controllers/EmailController.class.php';
+
+class VarifiedController extends BaseController {
+
+	function __construct (  ){
+		
+		parent::__construct();
+
+		$this -> logger -> debug("VarifiedController started");
+
+	}
+
+	function render (){
+		$baseUrl = $this->baseUrl;
+		$blueteamContactNumber = $this->blueteamContactNumber;
+		// here its shower that user is not in session
+		 
+		try{
+
+			require_once 'views/verified/blueteamverified.php';
+
+		} catch (Exception $e) {
+
+			//require_once 'views/error/pages-404.php';	
+			$this->logger->error( "Error occur :500 ".json_encode($e) );
+		}
+
+	}
+}
+?>
---
+++ src/views/verified/blueteamverified.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<!--[if IE 9]> <html class="ie9"> <![endif]-->
+<!--[if !IE]><!--><html lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow">
+
+    <title>BlueTeam | Awesome Hiring Services, Hire maid, cook, baby sitter, electrician, plumber, security guard, driver, gardener at Affordable price.</title>
+
+    <!-- for Google -->
+    <meta name="description" content="Hire high skilled, Background verified, experienced and certified professional services like maid, cook, electrician, plumber, baby sitter, gardener and more at affordable price." />
+    <meta name="keywords" content="Awesome hiring services, Hire, hire cook, hire maid, hire electrician, hire plumber, gurgaon, hire cook in gurgaon, blueteam, hire security guard" />
+    <meta name="author" content="BlueTeam" />
+    <meta name="copyright" content="true" />
+    <meta name="application-name" content="website" />
+
+    <!-- for Facebook -->          
+    <meta property="og:title" content="BlueTeam | Awesome Hiring Services, Hire maid, cook, baby sitter, electrician, plumber, security guard, driver, gardener at Affordable price." />
+    <meta name="og:author" content="BlueTeam" />
+    <meta property="og:type" content="website"/>
+
+    <meta name="p:domain_verify" content=""/>
+    <meta property="og:image" content='<?= $this-> baseUrl ?>static/images/logo.png' />
+    <meta property="og:url" content="<?= $this-> baseUrl ?>" />
+    <meta property="og:image:type" content="image/jpeg" />
+
+    <meta property="og:description" content="Hire high skilled, Background verified, experienced and certified professional services like maid, cook, electrician, plumber, baby sitter, gardener and more at affordable price." />
+
+    <!-- for Twitter -->          
+    <!-- <meta name="twitter:card" content="n/a" /> -->
+    <meta name="twitter:site" content="@hireblueteam">
+    <meta name="twitter:creator" content="@hireblueteam">
+    <meta name="twitter:url" content="<?= $this-> baseUrl ?>" />
+    <meta name="twitter:title" content="BlueTeam | Awesome Hiring Services, Hire maid, cook, baby sitter, electrician, plumber, security guard, driver, gardener at Affordable price." />
+    <meta name="twitter:description" content="Hire high skilled, Background verified, experienced and certified professional services like maid, cook, electrician, plumber, baby sitter, gardener and more at affordable price." />
+    <meta name="twitter:image" content="<?= $this-> baseUrl ?>static/images/logo.png" />
+    <!--[if IE]> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="<?= $this-> baseUrl ?>static/css/fonts.css">
+    <link rel="stylesheet" href="<?= $this-> baseUrl ?>static/css/bootstrap.min.css">
+       <!--  <link rel="stylesheet" href="<?= $this-> baseUrl ?>static/css/font-awesome.min.css">
+   --> 
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+   <link rel="stylesheet" href="<?= $this-> baseUrl ?>static/css/animate.css">
+   <link rel="stylesheet" href="<?= $this-> baseUrl ?>static/css/revslider2.css">
+   <link rel="stylesheet" href="<?= $this-> baseUrl ?>static/css/style.css">
+   <link rel="stylesheet" href="<?= $this-> baseUrl ?>static/css/responsive.css">
+
+   <!-- Favicons -->
+   <link rel="apple-touch-icon" sizes="57x57" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-57x57.png">
+   <link rel="apple-touch-icon" sizes="60x60" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-60x60.png">
+   <link rel="apple-touch-icon" sizes="72x72" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-72x72.png">
+   <link rel="apple-touch-icon" sizes="76x76" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-76x76.png">
+   <link rel="apple-touch-icon" sizes="114x114" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-114x114.png">
+   <link rel="apple-touch-icon" sizes="120x120" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-120x120.png">
+   <link rel="apple-touch-icon" sizes="144x144" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-144x144.png">
+   <link rel="apple-touch-icon" sizes="152x152" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-152x152.png">
+   <link rel="apple-touch-icon" sizes="180x180" href="<?= $this-> baseUrl ?>static/images/fevicon/apple-icon-180x180.png">
+   <link rel="icon" type="image/png" sizes="192x192"  href="<?= $this-> baseUrl ?>static/images/fevicon/android-icon-192x192.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="<?= $this-> baseUrl ?>static/images/fevicon/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="96x96" href="<?= $this-> baseUrl ?>static/images/fevicon/favicon-96x96.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="<?= $this-> baseUrl ?>static/images/fevicon/favicon-16x16.png">
+   <link rel="manifest" href="<?= $this-> baseUrl ?>static/images/fevicon/manifest.json">
+   <meta name="msapplication-TileColor" content="#ffffff">
+   <meta name="msapplication-TileImage" content="<?= $this-> baseUrl ?>static/images/fevicon/ms-icon-144x144.png">
+   <meta name="theme-color" content="#ffffff">
+
+   <!--- jQuery -->
+   <script src="<?= $this-> baseUrl ?>static/js/jquery.min.js"></script>
+
+   <!-- Queryloader -->
+   <script src="<?= $this-> baseUrl ?>static/js/queryloader2.min.js"></script>
+<?php /*
+   <!-- Modernizr -->
+   <script src="<?= $this-> baseUrl ?>static/js/modernizr.js"></script>
+*/?>
+
+</head>
+<body data-spy="scroll" data-target="#main-menu">
+    <div class="geass-loader-overlay left"></div><!-- End .geass-loader-overlay left -->
+    <div class="geass-loader-overlay right"></div><!-- End .geass-loader-overlay right -->
+    <div id="wrapper">
+
+
+        <?php include_once 'views/navbar/navbar.php'; ?>
+          <section id="services" class="section">
+                <header class="container text-center">
+                    <h3 class="fancy">BlueTeam Verified </h3><br/>
+                    <p align="left">
+                       
+                    BlueTeam is the blue collar job services provider for all type of domestic works like cooking, cleaning, housekeeping, plumbing, electrician, driver, baby care, elderly care etc.  
+                    <br/><br/>
+
+                    BlueTeam is highly committed to deliver best of the services for the customer. For this purpose, to provide a trustworthy, honest and experienced worker. BlueTeam has stringent process and specifications related to the domestic worker.
+                    <br/><br/>
+                    BlueTeam checks various aspects of a potential worker before recruiting him, so that s/he can deliver the best of the services. 
+                     <br/><br/>
+                    IF YOU DO NOT ACCEPT THE TERMS AND CONDITIONS OF USE STATED HEREIN, KINDLY REFRAIN FROM USING THE WEBSITE AND ITS SERVICES.
+                    <br/><br/>
+                    <h4 class="fancy">Verification Process of BlueTeam Employee/Domestic Worker</h4>
+                    <br/></p><p align="left">
+                    1) Collection of all the documents ( like Pan Card, Voter ID Card, Aadhar Card, Passport, Driver’s License ) 
+                    <br/><br/>
+                    2) Documents  and Information Verification 
+                    <br/><br/>
+                    3)Identity and local & hometown address verification
+                    <br/><br/>
+                    4) Character reference checks like criminal history
+                    <br/><br/>
+                    5) Educational documents checks
+                    <br/><br/>
+                    6) Physical visit to home
+                    <br/><br/> 
+                    7) Previous employment checks 
+                    <br/><br/>
+                    8) Experience/Skills checks
+                    <br/><br/>
+                    9) Interviewing candidates
+                    <br/><br/>
+                    <h4 class="fancy">After all these steps, if person find suitable for the particular job then s/he send to the customer. Various aspects has been followed from customer side like-</h4>
+                    <br/></p><p align="left">
+                    1) Phone interview 
+                    <br/><br/>
+                    2) Appointment with the BlueTeam employee to view candidates files 
+                    <br/><br/>
+                    3) Making of appointments for selected candidates for interview
+                    <br/><br/>
+                    4) Customer choose candidates for trial/demo
+                    <br/><br/>
+                    5) Provide information for successful placement of the candidate
+                    <br/><br/>
+                    6) Police varification of the candidate
+                    <br/><br/>
+                    </p>
+                </header>
+
+            </section>
+  
+           <!-- Contact Us Section -->
+            <section id="contactus" class="section">
+                <header class="container text-center">
+                    <h1 class="section-title fancy">Contact Us</h1>
+                    <p class="section-desc">
+                        Get in touch with us. We want to hear from you. Let's discuss how can we provide better support and services...
+                    </p>
+                </header>
+                <div id="map"></div><!-- End #map -->
+
+                <div class="lg-margin2x"></div><!-- space -->
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4">
+                            <div class="contact-info-box box yellow text-center">
+                                <h3>Addresses</h3>
+                                <span class="contact-info-icon yellowbg"><i class="fa fa-map-marker"></i></span>
+                                <ul class="contact-info-list">
+                                    <li>Sector 31</li>
+                                    <li>Gurgaon, India</li>
+                                </ul>
+                            </div><!-- End .contact-info-box -->
+                        </div><!-- End .col-md-4 -->
+
+                        <div class="col-md-4 col-sm-4">
+                            <div class="contact-info-box box lightblue text-center">
+                                <h3>Call Us</h3>
+                                <span class="contact-info-icon lightbluebg"><i class="fa fa-mobile-phone"></i></span>
+                                <ul class="contact-info-list">
+                                    <!-- <li>8901414422</li> -->
+                                    <li><?= $this-> blueteamContactNumber ?></li>
+                                    <li> +91 8901414422</li>
+                                </ul>
+                            </div><!-- End .contact-info-box -->
+                        </div><!-- End .col-md-4 -->
+
+                        <div class="col-md-4 col-sm-4">
+                            <div class="contact-info-box box red text-center">
+                                <h3>Quick Contact</h3>
+                                <span class="contact-info-icon redbg"><i class="fa fa-envelope-o"></i></span>
+                                <ul class="contact-info-list">
+                                    <li><a href="mailto:hire@blueteam.in">hire@blueteam.in</a></li>
+                                    <li><em class="highlight red">Twitter:</em> hireblueteam</li>
+                                </ul>
+                            </div><!-- End .contact-info-box -->
+                        </div><!-- End .col-md-4 -->
+                    </div><!-- End .row -->
+                </div><!-- End .container -->
+
+            </section>
+
+            <footer id="footer" class="parallax" data-stellar-background-ratio="0.15">
+                <div class="overlaybg overlay-pattern1"></div><!-- End .section-overlay -->
+                <div class="section-content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <a href="#home" class="footer-logo" title="BlueTeam | Hire now"><img src="<?= $this -> baseUrl ?>static/images/logo.png" width="210" alt="BlueTeam"></a>
+                            </div><!-- End .col-md-12 -->
+                        </div><!-- End .row -->
+                    </div><!-- End .container -->
+
+                    <div class="fb-like" data-href="https://www.facebook.com/blueteam.in" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+
+                    <div class="footer-social-icons transparent">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <ul class="social-icons-container">
+                                        <li><a href="https://www.facebook.com/blueteam.in" class="facebook add-tooltip" data-placement="top" data-toggle="tooltip" target="_blank" title="Follow us on Facebook"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="https://twitter.com/hireblueteam" class="twitter add-tooltip" data-placement="top" data-toggle="tooltip" target="_blank" title="Follow us on Twitter"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#" class="googleplus add-tooltip" data-placement="top" data-toggle="tooltip" target="_blank" title="Follow us on Google +"><i class="fa fa-google-plus"></i></a></li>
+                                        <!-- <li><a href="index14.html#" class="dribbble add-tooltip" data-placement="top" data-toggle="tooltip" title="Find us at Dribbble"><i class="fa fa-dribbble"></i></a></li>
+                                        <li><a href="index14.html#" class="tumblr add-tooltip" data-placement="top" data-toggle="tooltip" title="Find us at Tumblr"><i class="fa fa-tumblr"></i></a></li>
+                                        <li><a href="index14.html#" class="flickr add-tooltip" data-placement="top" data-toggle="tooltip" title="Find us at Flickr"><i class="fa fa-flickr"></i></a></li> -->
+                                    </ul>
+                                </div><!-- End .col-md-12 -->
+                            </div><!-- End .row -->
+                        </div><!-- End .container -->
+                    </div><!-- End .footer-social-icons -->
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p>All rights reserved &copy; <a href="http://shatkonlabs.com" class="yellow-color" title="Shatkon Labs" target="_blank">Shatkon Labs&trade;</a></p>
+                                <span class="footer-date highlight red">2015</span>
+                            </div><!-- End .col-md-12 -->
+                        </div><!-- End .row -->
+                    </div><!-- End .container -->
+                </div><!-- End .section-content -->
+            </footer>
+            
+        </div><!-- End #wrapper -->
+
+        <!-- Scroll Top Button -->
+        <a href="#" id="scroll-top" class="add-tooltip" data-placement="top" title="Go to top"><i class="fa fa-angle-double-up"></i></a>
+
+</div>
+</div>
+
+<?php include_once 'views/footer/footer.php'; ?>        
+
+
+</body>
+</html>
