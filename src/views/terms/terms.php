7ac6b70b4cfe97f9c75781d2e096034bd50070e9
anil-ninja <kumar.anil8892@gmail.com>
2016-06-13 11:24:35 +0530

    icons and tnc

--- src/views/terms/terms.php
+++ src/views/terms/terms.php
@@ -90,59 +90,67 @@
                 <header class="container text-center">
                     <h3 class="fancy">TERMS AND CONDITIONS</h3><br/>
                     <p align="left">
-                       
-
-                    PLEASE READ THE TERMS AND CONDITIONS OF USE CAREFULLY. BY USING www.blueteam.in  AND ITS SERVICES YOUR AGREE TO BE BOUND BY THE PROVISIONS OF THE TERMS AND CONDITIONS AND CONTINUING USAGE FOLLOWING ANY SUCH REVISIONS TO THE TERMS AND CONDITIONS CONSTITUTES YOUR AGREEMENT TO BE BOUND BY THE TERMS OF USE AS MODIFIED FROM TIME TO TIME. 
+                    PLEASE READ THE TERMS AND CONDITIONS OF USE CAREFULLY. BY USING www.blueteam.in  AND ITS SERVICES YOUR AGREE TO BE BOUND BY THE PROVISIONS OF THE TERMS AND CONDITIONS AND CONTINUING USAGE FOLLOWING ANY SUCH REVISIONS TO THE TERMS AND CONDITIONS CONSTITUTES YOUR AGREEMENT TO BE BOUND BY THE TERMS OF USE AS MODIFIED FROM TIME TO TIME.
                     <br/><br/>
 
                     SHATKON LABS PRIVATE LIMITED , AN INDIAN COMPANY INCORPORATED UNDER THE COMPANIES ACT OF 2013 OWNS AND OPERATES THE DOMAIN NAME www.blueteam.in   AND RESERVES THE RIGHT TO REVISE THESE TERMS AND CONDITIONS OF USE, INCLUDING THE PRIVACY STATEMENT. THE COMPANY DOES NOT HAVE ANY OBLIGATION OR RESPONSIBILITY TO INFORM OR INTIMATE THE USER IN ANY MANNER, DIRECTLY OR INDIRECTLY, OF SUCH REVISIONS EITHER AT THE TIME OF MAKING THEM OR THEREAFTER.
                     <br/><br/>
-                    ANY ADDITIONAL TERMS AND CONDITIONS, DISCLAIMERS, PRIVACY POLICIES AND OTHER POLICIES APPLICABLE TO GENERAL AND SPECIFIC AREAS OF THIS WEB SITE OR TO PARTICULAR CONTENT ARE ALSO CONSIDERED AS TERMS OF USE. THE USER SHOULD VISIT THIS PAGE PERIODICALLY TO REVIEW THE TERMS OF USE, BECAUSE THEY ARE BINDING ON YOU. THE TERMS "USER" AS USED HEREIN REFER TO ALL INDIVIDUALS AND/OR ENTITIES ACCESSING THE WEBSITE FOR ANY REASON.
-                     <br/><br/>
-                    IF YOU DO NOT ACCEPT THE TERMS AND CONDITIONS OF USE STATED HEREIN, KINDLY REFRAIN FROM USING THE WEBSITE AND ITS SERVICES.
+
+                    ANY ADDITIONAL TERMS AND CONDITIONS, DISCLAIMERS, PRIVACY POLICIES AND OTHER POLICIES APPLICABLE TO GENERAL AND SPECIFIC AREAS OF THIS WEB SITE OR TO PARTICULAR CONTENT ARE ALSO CONSIDERED AS TERMS OF USE. THE USER SHOULD VISIT THIS PAGE PERIODICALLY TO REVIEW THE TERMS OF USE, BECAUSE THEY ARE BINDING ON YOU. THE TERMS 'USER' AS USED HEREIN REFER TO ALL INDIVIDUALS AND/OR ENTITIES ACCESSING THE WEBSITE FOR ANY REASON.
                     <br/><br/>
 
-                    These Terms of Use include the BlueTeam.in Privacy Statement (https://www.BlueTeam.in/Privacy) which is incorporated into these Terms of Use by reference. By using BlueTeam.in, You are indicating your acceptance to be bound by these Terms of Use, including the BlueTeam.in Privacy Statement.
-                    <br/><br/>
+                    These Terms of Use include the BlueTeam.in Privacy Statement (http://www.BlueTeam.in/Privacy) which is incorporated into these Terms of Use by reference. By using BlueTeam.in, You are indicating your acceptance to be bound by these Terms of Use, including the BlueTeam.in Privacy Statement.
+                    <br/>
                     In the event of any inconsistency between the provisions under the Terms of Use and the BlueTeam.in Privacy Statement, the provisions under the Terms of Use will prevail.
-                    <br/>
-                    <br/>
-
+                    <br/><br/>
+                    
+                    Shatkon Labs Private Limited, a company incorporated under the Indian Companies Act, 1956, is operating the app \“BlueTeam\” and website www.blueteam.in.<br/>
+                    This page states the terms of use (\"Terms of Use\" / \“Terms\”) under which you (\“You\” / \“Your\”) may use BlueTeam, an on-line service to provide domestic helper.<br/><br/>
                     <h4 class="fancy">TERMS OF USE</h4>
-                    <br/></p><p align="left">
-                    1) BlueTeam.in charges a fees for it’s placement services which is equivalent to the salary payable to the placed individual for his/her services for a period of one month. Taxes as may be applicable shall be payable separately by the user. 
+                    <br/>
+                  
+                    1) BlueTeam.in don\'t charges service charge or commission, monthly cost of consuming BlueTeam Services should be paid in advance. Taxes as may be applicable shall be payable separately by the user.
                     <br/><br/>
-                    2) The placed individual may be replaced by another individual of the user's choice after a minimum of 6 months with a maximum of 3 such replacements in a period of 6 months. 
+                    2) In a particular month Customer have to pay salary/charges of the domestic helper (the word \“Domestic Helper\” refer to cook, maid, driver, babysitter, patient care, elderly care) at least minimum of 21 days and maximum of 26 days according to charges applicable.
                     <br/><br/>
-                    3) A Renewal fees shall be payable after first six months, and the quantum shall be equivalent to  50% of the placed individual's salary for period of next 6 months.
+                    3) If the Customer want to take only 15 days service in a month, then minimum of 21 days charges would have to pay to the company. If Customer want to take the service less than 15 days, in that case BlueTeam On demand charges would be applicable.
                     <br/><br/>
                     4) The client undertakes to notify BlueTeam.in immediately upon agreeing to hire a candidate introduced by BlueTeam.in
                     <br/><br/>
-                    5) An invoice will be sent to the client upon commencement of employment and the placement fee is payable immediately on the date of joining of the candidate
+                    5) An invoice will be sent to the client upon commencement of employment and the salary/charges fee would be payable immediately or on the date of joining of the candidate.
                     <br/><br/>
-                    6) Introductions are strictly confidential. If BlueTeam.in introduces a candidate that the client then introduces to a third part, who hires the candidate on either full time or part time, the client agrees to pay BlueTeam.in the placement charges and taxes as applicable.
-                    <br/><br/> 
-                    7) While it is the BlueTeam’s endeavour to introduce the best suited candidates to the client, it shall be the responsibility of the client to satisfy themselves of the suitability of an applicant before hiring them. If required the client may arrange for an interview and medical examination of the candidate prior to hiring them. BlueTeam.in shall assist as far as practicable to facilitate the process. 
+                    6) The Customer should have to inform at least one day prior to the \“Company\” if Customer do not need the service for any particular day/days, otherwise salary/charges would be levied according to the plan.
                     <br/><br/>
-                    8) Prior to the placement of the candidate the client shall share a job description detailing the tasks which are expected to be performed by the candidate upon placement. The client shall be bound by the said description and shall not engage the placed individual in tasks which fall outside the purview of those clearly stated in the job description. BlueTeam shall not be liable for any conflict with respect to tasks assigned which do not clearly fall within the scope of the said description. The placed individual shall be at the liberty to terminate the employment contract upon establishing that tasks assigned to him/her fell squarely outside the scope of the description. In such a case BlueTeam shall not be liable to provide for a replacement  and neither refund the placement charges paid by the client. 
+                    7) The Customer should have to pay in cash or transfer the salary/charges in advance to the company for the next month after the completion of the previous service month, if fail to do so, 2% of the interest charges would be levied as late payment charges after the 7 days of the payment due date.
                     <br/><br/>
-                    9) Despite best efforts to ensure that the a thorough and deligent background verification of the candidate/placed individual is perfomed, BlueTeam strongly suggests that the client either himself or ask BlueTeam.in to do a POLICE VERIFICATION of the worker. This ensures that the worker is free of any criminal charges in the past. If BlueTeam.in is asked to do a police verification, then it will chargeable as per actuals.
+                    8) Domestic helper should be provided at least two holidays in a particular month.
+                    <br/><br/>
+                    9) The placed individual may be replaced by another individual of the user's choice after a minimum of 15 day of the service.
+                    <br/><br/>
+                    10) If Customer want to take the domestic helper from BlueTeam, then customer have to pay 500 Rs booking charges in advance to the company for first month only, which is non refundable, If customer take the service then booking charges would be add to the first month salary of the domestic helper, otherwise booking charges would forfeited.
+                    <br/><br/>
+                    11) An invoice will be sent to the client upon commencement of employment and the placement fee is payable immediately on the date of joining of the candidate.
+                    <br/><br/>
+                    12) Introductions are strictly confidential. If BlueTeam.in introduces a candidate that the client then introduces to a third part, who hires the candidate on either full time or part time, the client agrees to pay BlueTeam.in the placement charges and taxes as applicable.
+                    <br/><br/>
+                    13) While it is the BlueTeam’s endeavour to introduce the best suited candidates to the client, it shall be the responsibility of the client to satisfy themselves of the suitability of an applicant before hiring them. If required the client may arrange for an interview and medical examination of the candidate prior to hiring them. BlueTeam.in shall assist as far as practicable to facilitate the process.
+                    <br/><br/>
+                    14) Prior to the placement of the candidate the client shall share a job description detailing the tasks which are expected to be performed by the candidate upon placement. The client shall be bound by the said description and shall not engage the placed individual in tasks which fall outside the purview of those clearly stated in the job description. BlueTeam shall not be liable for any conflict with respect to tasks assigned which do not clearly fall within the scope of the said description. The placed individual shall be at the liberty to terminate the employment contract upon establishing that tasks assigned to him/her fell squarely outside the scope of the description. In such a case BlueTeam shall not be liable to provide for a replacement  and neither refund the placement charges paid by the client.
+                    <br/><br/>
+                    15) Despite best efforts to ensure that the a thorough and deligent background verification of the candidate/placed individual is perfomed, BlueTeam strongly suggests that the client either himself or ask BlueTeam.in to do a POLICE VERIFICATION of the worker. This ensures that the worker is free of any criminal charges in the past. If BlueTeam.in is asked to do a police verification, then it will chargeable as per actuals. BlueTeam shall not be responsible or liable for any act or omission, either Civil or Criminal in nature, of the placed candidate.
+                    <br/><br/>
+                    16) If the invoices are settled and due to reason other than those mentioned in Clause 8,  the candidate resigns or otherwise fails to report for work for a prolonged period without prior notice,  a free replacement shall be provided at no extra cost. Usually the time taken for the replacement is 7 days but it may vary depending on the availability.
+                    <br/><br/>
+                    17) In case where the placed individual has been hired part-time (Less than 24 hours) the client shall perform due diligence and perform a checking of the objects carried by the placed individual while entering and leaving the premises of the client. This is only for the safety of the client. If the placed individual is found to carry something objectionable, the client shall immediately notify BlueTeam.
+                    <br/><br/>
+                    18) The client agrees that the worker shall not be subjected to harm, mental or physical and shall be legally liable for such acts. If the placed individual leaves due to any such reason, BlueTeam.in will not replace the worker and neither refund the placement charges paid by the client.
+                    <br/><br/>
+                    19) All payments shall to be made in favour of 'SHATKON LABS Pvt Ltd'
+                    <br/><br/>
+                    20) All disputes shall be subject to Delhi Jurisdiction only.
+                    <br/><br/>
+                    21) If due to any circumstance where a refund becomes due to the client, the amount shall be refunded within 90 days of the candidate leaving the job.
                     <br/>
-                    BlueTeam shall not be responsible or liable for any act or omission, either Civil or Criminal in nature, of the placed candidate. 
-                    <br/><br/>
-                    10) If the invoices are settled and due to reason other than those mentioned in Clause 8,  the candidate resigns or otherwise fails to report for work for a prolonged period without prior notice,  a free replacement shall be provided at no extra cost. Usually the time taken for the replacement is 7 days but it may vary depending on the availability.
-                    <br/><br/>
-                    11) In case where the placed individual has been hired part-time (Less than 24 hours) the client shall perform due diligence and perform a checking of the objects carried by the placed individual while entering and leaving the premises of the client. This is only for the safety of the client. If the placed individual is found to carry something objectionable, the client shall immediately notify BlueTeam. 
-                    <br/><br/>
-                    12) The client agrees that the worker shall not be subjected to harm, mental or physical and shall be legally liable for such acts. If the placed individual leaves due to any such reason, BlueTeam.in will not replace the worker and neither refund the placement charges paid by the client. 
-                    <br/><br/>
-                    13) All payments shall to be made in favour of ‘SHATKON LABS Pvt Ltd’
-                    <br/><br/>
-                    14) All disputes shall be subject to Delhi Jurisdiction only. 
-                    <br/><br/>
-                    15) If due to any circumstance where a refund becomes due to the client, the amount shall be refunded within 90 days of the candidate leaving the job.
-                    <br/>
-
                     </p>
                 </header>
 
