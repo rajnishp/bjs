2b77865712873b90aff938ac8f1122a2aa26a87f
anil-ninja <kumar.anil8892@gmail.com>
2016-09-29 17:02:31 +0530

    livechat

--- src/views/footer/footer.php
+++ src/views/footer/footer.php
@@ -368,7 +368,7 @@ String.prototype.capitalizeFirstLetter = function() {
 
 <script type="text/javascript">
 var LHCChatOptions = {};
-LHCChatOptions.opt = {widget_height:340,widget_width:300,popup_height:520,popup_width:500};
+LHCChatOptions.opt = {widget_height:340,widget_width:300,popup_height:520,popup_width:500,domain:'blueteam.in'};
 (function() {
 var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
 var referrer = (document.referrer) ? encodeURIComponent(document.referrer.substr(document.referrer.indexOf('://')+1)) : '';
