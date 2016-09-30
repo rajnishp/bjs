6f854662cec8467e23cca0b8632fecb65a03fbc5
anil-ninja <kumar.anil8892@gmail.com>
2016-09-30 15:52:00 +0530

    price table

--- src/views/landing/index.php
+++ src/views/landing/index.php
@@ -325,7 +325,7 @@
                                 <p style="font-size:30px;color:#fff;">Monthly & On-demand Domestic Help <br/>
                                 <a <?= $this-> bookLink ?> class="btn btn-lightblue" >Book Now</a></p>
                                 <!-- End .service -->
-                                <table style="font-size:16px;color:#fff;border:1px;">
+                                <table class="bordered" style="font-size:24px;color:#fff;">
                                     <thead>
                                         <tr>
                                             <th>Hours</th>
@@ -394,7 +394,7 @@
                                         </tr>
                                     </tbody>
                                 </table>
-                                <p class="section-title fancy" style="font-size:8px;color:#fff;">*Monthly bases charges</p>
+                                <p  style="font-size:8px;color:#fff;">*Monthly based charges</p>
                             </div><!-- End .col-md-3 -->
 
                             
